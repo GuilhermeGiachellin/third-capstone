d9663bdb02ce7f5eaee289fa12822a80473e5739
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-09-14 15:01:41 -0300

    Basic layout and styling

---
+++ src/components/details.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import './styles/details.css';
+
+const Details = () => (
+  <div>
+    <div className="headline">
+      Something
+    </div>
+    <div className="stats_title_cnt">
+      <h4>STATS BY CIV</h4>
+    </div>
+    <ul>
+      <li>Something</li>
+      <li>Somenthing</li>
+      <li>Something</li>
+    </ul>
+  </div>
+);
+
+export default Details;
