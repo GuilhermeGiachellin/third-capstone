01f9bdef48495e473d3a7ff4af7bd42c5c5d8694
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-09-16 23:40:21 -0300

    Added Redux and details page
    -Added Redux
    -Details page
    -Filter

--- src/components/home.js
+++ src/components/home.js
@@ -1,31 +1,75 @@
-/* eslint-disable no-unused-vars */
-import { useDispatch, useSelector } from 'react-redux';
-import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
 import './styles/home.css';
+import { FiArrowRightCircle } from 'react-icons/fi';
+import { NavLink } from 'react-router-dom';
+import { getFiltered } from '../redux/leaderboard/leaderboardRedux';
+import knight from './assets/knight.gif';
 
-const Home = () => (
-  <div>
-    <div className="headline">
-      Something
+const Home = () => {
+  const leaderboardList = useSelector((state) => state.leaderboardReducer);
+  const dispatch = useDispatch();
+
+  const displayPlayers = () => (
+    (leaderboardList.leaderboard.map((leaderboard) => (
+      <div key={leaderboard.id} id={leaderboard.id} className="content">
+        <NavLink key={leaderboard.id} to={`/${leaderboard.name}`}>
+          <FiArrowRightCircle className="arrow" />
+        </NavLink>
+        <h3 className="player_name">{leaderboard.name}</h3>
+        <h4 className="player_mmr">
+          MMR:
+          {leaderboard.details.rating}
+        </h4>
+      </div>
+    )))
+  );
+
+  const displayFiltered = () => (
+    (leaderboardList.filtered.map((leaderboard) => (
+      <div key={leaderboard.id} id={leaderboard.id} className="content">
+        <NavLink key={leaderboard.id} to={`/${leaderboard.name}`}>
+          <div className="arrow">
+            <FiArrowRightCircle />
+          </div>
+        </NavLink>
+        <h3 className="player_name">{leaderboard.name}</h3>
+        <h4 className="player_mmr">
+          MMR:
+          {leaderboard.details.rating}
+        </h4>
+      </div>
+    )))
+  );
+
+  const searchPlayer = (e) => {
+    const saveList = leaderboardList;
+    const searchString = e.target.value.toUpperCase();
+    const newArray = leaderboardList.leaderboard.filter((player) => (
+      player.name.toUpperCase().includes(searchString)
+    ));
+    dispatch(getFiltered(newArray, saveList));
+  };
+
+  return (
+    <div>
+      <div className="headline">
+        <img className="knight" src={knight} alt="knight" />
+        <h1>Current Top 10 AOE2:DE Players</h1>
+        <img className="knight" src={knight} alt="knight" />
+      </div>
+      <div className="stats_title_cnt">
+        <input placeholder="Player name" onChange={searchPlayer} />
+      </div>
+      <div className="content_cnt">
+        {
+        leaderboardList.filtered.length === 0
+          ? displayPlayers(leaderboardList)
+          : displayFiltered(leaderboardList)
+      }
+      </div>
     </div>
-    <div className="stats_title_cnt">
-      <h4>STATS BY CIV</h4>
-    </div>
-    <div className="content_cnt">
-      <div className="content1">
-        something
-      </div>
-      <div className="content2">
-        something
-      </div>
-      <div className="content2">
-        something
-      </div>
-      <div className="content1">
-        something
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,43 +1,26 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { HiCog, HiMicrophone } from 'react-icons/hi';
 import { IoIosArrowBack } from 'react-icons/io';
-import Home from './home';
-import Details from './details';
+
 import './styles/navBar.css';
 
 const NavBar = () => (
-  <Router>
-    <header>
-      <nav className="navBarHeader">
-        <div className="arrow_cnt">
-          <NavLink to="/details">
-            <IoIosArrowBack />
-          </NavLink>
-          <h4 className="year">2021</h4>
-        </div>
-        <h4 className="app_name">Name of APP</h4>
-        <div className="cog_cnt">
-          <HiMicrophone />
-          <HiCog />
-        </div>
-      </nav>
-    </header>
-
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/details">
-        <Details />
-      </Route>
-    </Switch>
-  </Router>
+  <header>
+    <nav className="navBarHeader">
+      <div className="arrow_cnt">
+        <NavLink to="/">
+          <IoIosArrowBack />
+        </NavLink>
+        <h4 className="year">2021</h4>
+      </div>
+      <h4 className="app_name">AOE2:DE Top 10</h4>
+      <div className="cog_cnt">
+        <HiMicrophone />
+        <HiCog />
+      </div>
+    </nav>
+  </header>
 );
 
 export default NavBar;
